4233f82aaf7074ac4ff5a6a08501d41b2db58153
sou24vik <dassouvik188@gmail.com>
2024-06-13 18:25:32 +0000

    function, scope, arrow, iife

--- basics/3_basic.js
+++ basics/3_basic.js
@@ -40,3 +40,101 @@ function sayMyName() {
 
   //  .......... Scopes ..........
 
+  function one() {
+    const name = "Monalisha"
+
+    function two() {
+        const age = "20"
+        console.log(name);
+
+    }
+
+    two()
+
+}
+one()
+
+
+if (true) {
+    const username = "mona"
+    if (username === "mona") {
+        const website = " google"
+        console.log(username + website);
+    }
+    console.log(username);
+}
+
+// console.log(username);
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(2));
+
+
+// .......... Arrow ..........
+
+const user = {
+    username: "mona",
+    password: "999@mona",
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
+
+console.log(this);
+
+
+// const subscriber = function() {
+//     let username = "mona"
+//     console.log(this.username);
+// }
+
+
+
+const subscriber = () => {
+    let username = "mona"
+    console.log(this);
+}
+
+subscriber()
+
+// const addTwoNo = (num1, num2) => {
+//          return num1 + num2
+//      }
+    
+     //const addTwoNo = (num1, num2) =>  num1 + num2
+    
+     //const addTwoNo = (num1, num2) => ( num1 + num2 )
+
+     const addTwoNo = (num1, num2) => ({username: "mona"})
+
+
+    console.log(addTwoNo(5,6))
+
+    // .......... IIFE ............
+
+    // Immediately Invoked Function Expressions (IIFE)
+
+
+(function customer() {
+    console.log(`DB CONNECTED`);
+ })();
+
+ ((customer) => {
+    console.log(`DB CONNECTED TWO ${customer}`);
+ })("souvik")
